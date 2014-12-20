243fa9e0d80c800e32c5c7a9b9a61c3f60a65cd7
kelseyljacobsen <kelseyljacobsen@gmail.com>
2014-12-19 17:30:25 -0800

    now with firebase

--- js/app.js
+++ js/app.js
@@ -1,296 +1,301 @@
-var ticTacToeApp = angular.module("ticTacToeApp",['firebase']);
+angular
+	.module('ticTacToeApp')
+	.controller('TicTacToeController', TicTacToeController);
 
-ticTacToeApp
+	TicTacToeController.$inject = ['$firebase'];
 
-	.controller("TicTacToeController", ['$scope', '$firebase', function($scope, $firebase){
+	function TicTacToeController($firebase) {
+		var controller = this;
+
+		controller.game = getGameObject();
+		controller.getGameWindow = getGameWindow;
+		controller.submit = submit;
+		controller.startGame = startGame;
+		controller.resetGame = resetGame;
+		controller.playAgain = playAgain;
+		controller.makeMove = makeMove;
+		controller.compare = compare;
+		
+		// controller.welcomeWindow = true;
+		// controller.gameWindow = false;
+
+			// 	$scope.triggerInfoBox = function(){
+			// $scope.infoBoxView = true;
+
+		
+		controller.welcomeWindow = true;
+		controller.gameWindow = false;	
+
+
+		function getGameObject() {
+			var ref = new Firebase('https://chalkboardtictactoe.firebaseio.com/game');
+			return $firebase(ref).$asObject();
+		}
+
+		function getGameWindow() {
+			controller.welcomeWindow = false;
+			controller.gameWindow = true;
+			newGame();
+
+			controller.game.$save();
+		}
 
 //-------This sets the board up so a game can begin-----------------------------------------------------------//
 
-	$scope.squares = [
-		[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}],
-		[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}],
-		[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}]
-	];
+		function newGame() {
+			controller.game.squares = [
+				[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}],
+				[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}],
+				[{owner:"", color:false},{owner:"", color:false},{owner:"", color:false}]
+			];
 
-	$scope.Player_Name_One = "";
-	$scope.Player_Name_Two = "";
-	$scope.catsGame = false;
-	$scope.isDisabled = true; 
+			controller.game.Player_Name_One = "";
+			controller.game.Player_Name_Two = "";
+			controller.game.helloOne = false;
+			controller.game.helloTwo = false;
+			controller.game.catsGame = false;
+			controller.game.isDisabled = true; 
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
+			controller.game.winnerOne = false;
+			controller.game.winnerTwo = false;
+			controller.game.formOne = false;
+			controller.game.formTwo = false;
+			controller.game.Player_Turn = "Player 1";
+			
+			controller.game.$save();
+		};
 
-// $scope.game = getGame();
 
-// function getGame() {
-// 	var ref = new Firebase("cite from online/where you want to store it");
-// 	var game = $firebase(ref).$asObject();
-// 	return game;
-// }
-
-	// $scope.game = {squares: [
-	// 		[{owner:""},{owner:""},{owner:""}],
-	// 		[{owner:""},{owner:""},{owner:""}],
-	// 		[{owner:""},{owner:""},{owner:""}]
-	// 	],
-
-	// 	Player_Name_One: "",
-	// 	Player_Name_Two: "",
-	// 	catsGame: false,
-	// 	isDisabled: true 
-	// };
-
-	//$scope.game.$save(); <--need to add this to every function (and add $scope.game to all)
 
 //-------This is where players enter their names and player turns are assigned--------------------------------//
 
-	$scope.submit = function(player) {
-		if (player==1) {
-			$scope.formOne = true;
-			$scope.helloOne = true;
-		}
-		else if (player==2) {
-			$scope.formTwo = true;
-			$scope.helloTwo = true;
-		}
-	
-		$scope.Player_Name_One = $scope.inputOne;
-		$scope.Player_Name_Two = $scope.inputTwo;
-	};
+		function submit(player) {
+			if (player==1) {
+				controller.game.formOne = true;
+				controller.whichSide = 'Player 1';
+				controller.game.helloOne = true;
+				controller.game.Player_Name_One = controller.inputOne;
+			}
+			else if (player==2) {
+				controller.game.formTwo = true;
+				controller.whichSide = 'Player 2';
+				controller.game.helloTwo = true;
+				controller.game.Player_Name_Two = controller.inputTwo;
+			};
+
+			controller.game.$save();
+		};
 
 //-------This is where the game is set. Play can't start until "let's play!" button is pressed---------------//
 //-------Player 1 always plays first-------------------------------------------------------------------------//
 
-	$scope.startGame = function() {
-		$scope.yourTurnOne = true;
-		$scope.yourTurnTwo = false;
-		$scope.helloOne = false;
-		$scope.helloTwo = false;
-		$scope.isDisabled = false;
-	};
+		function startGame() {
+			controller.game.yourTurnOne = true;
+			controller.game.helloOne = false;
+			controller.game.helloTwo = false;
+			controller.game.isDisabled = false;
 
-	$scope.Player_Turn = "Player 1";
+			controller.game.$save();
+		};
+
+	
 
 //-------This is where players make their moves and claim squares--------------------------------------------//
 //-------The compare function is automatically invoked after square is claimed-------------------------------//
 
-	$scope.makeMove = function(square){
-		if (square.owner == "P1" || square.owner == "P2") {
-			alert("Sorry, that square is already taken!");
-		} 
-		else if ($scope.Player_Turn == "Player 1") {
-			square.owner = "P1";
-			$scope.Player_Turn = "Player 2";
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = true;
-			$scope.compare();
-		} 
-		else if ($scope.Player_Turn == "Player 2") {
-			square.owner = "P2";
-			$scope.Player_Turn = "Player 1";
-			$scope.yourTurnTwo = false;
-			$scope.yourTurnOne = true;
-			$scope.compare();
-		}
+		function makeMove(square) {
+			if (square.owner == "P1" || square.owner == "P2") {
+				alert("Sorry, that square is already taken!");
+			} 
+			else if (controller.game.Player_Turn == 'Player 1' && controller.whichSide == 'Player 1') {
+				square.owner = "P1";
+				controller.game.Player_Turn = "Player 2";
+				controller.game.yourTurnOne = false;
+				controller.game.yourTurnTwo = true;
+				controller.compare();
+			} 
+			else if (controller.game.Player_Turn == 'Player 2' && controller.whichSide == 'Player 2') {
+				square.owner = "P2";
+				controller.game.Player_Turn = "Player 1";
+				controller.game.yourTurnTwo = false;
+				controller.game.yourTurnOne = true;
+				controller.compare();
+			}
 
-		// $scope.game.$save();
-	};
+			controller.game.$save();
+		};
 
 //-------After move is made, the game compares the whole board at once, looking for winning rows/columns-----//
 
-	$scope.compare = function(){
-		var square1 = $scope.squares[0][0];
-		var square2 = $scope.squares[0][1];
-		var square3 = $scope.squares[0][2];
-		var square4 = $scope.squares[1][0];
-		var square5 = $scope.squares[1][1];
-		var square6 = $scope.squares[1][2];
-		var square7 = $scope.squares[2][0];
-		var square8 = $scope.squares[2][1];
-		var square9 = $scope.squares[2][2];
-		
-		$scope.winnerOne = false;
-		$scope.winnerTwo = false;
-		$scope.catsGame = false;
-		$scope.isDisabled = false;
+		function compare() {
+			var square1 = controller.game.squares[0][0];
+			var square2 = controller.game.squares[0][1];
+			var square3 = controller.game.squares[0][2];
+			var square4 = controller.game.squares[1][0];
+			var square5 = controller.game.squares[1][1];
+			var square6 = controller.game.squares[1][2];
+			var square7 = controller.game.squares[2][0];
+			var square8 = controller.game.squares[2][1];
+			var square9 = controller.game.squares[2][2];
 
 		if (square1.owner == "P1" && square2.owner == "P1" && square3.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square1.color = square2.color = square3.color = true;
 		} 
 		else if (square4.owner == "P1" && square5.owner == "P1" && square6.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square4.color = square5.color = square6.color = true;
 		} 
 		else if (square7.owner == "P1" && square8.owner == "P1" && square9.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square7.color = square8.color = square9.color = true;
 		} 
 		else if (square1.owner == "P1" && square4.owner == "P1" && square7.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square1.color = square4.color = square7.color = true;
 		} 
 		else if (square2.owner == "P1" && square5.owner == "P1" && square8.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square2.color = square5.color = square8.color = true;
 		} 
 		else if (square3.owner == "P1" && square6.owner == "P1" && square9.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square3.color = square6.color = square9.color = true;
 		} 
 		else if (square1.owner == "P1" && square5.owner == "P1" && square9.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square1.color = square5.color = square9.color = true;
 		} 
 		else if (square3.owner == "P1" && square5.owner == "P1" && square7.owner == "P1"){
-			$scope.winnerOne = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerOne = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square3.color = square5.color = square7.color = true;
 		} 
 		else if (square1.owner == "P2" && square2.owner == "P2" && square3.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
-			$scope.winningPlay = [1,2,3];
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
+			controller.game.winningPlay = [1,2,3];
 			square1.color = square2.color = square3.color = true;
 		} 
 		else if (square4.owner == "P2" && square5.owner == "P2" && square6.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square4.color = square5.color = square6.color = true;
 		} 
 		else if (square7.owner == "P2" && square8.owner == "P2" && square9.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square7.color = square8.color = square9.color = true;
 		} 
 		else if (square1.owner == "P2" && square4.owner == "P2" && square7.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square1.color = square4.color = square7.color = true;
 		} 
 		else if (square2.owner == "P2" && square5.owner == "P2" && square8.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square2.color = square5.color = square8.color = true;
 		} 
 		else if (square3.owner == "P2" && square6.owner == "P2" && square9.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square3.color = square6.color = square9.color = true;
 		} 
 		else if (square1.owner == "P2" && square5.owner == "P2" && square9.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square1.color = square5.color = square9.color = true;
 		} 
 		else if (square3.owner == "P2" && square5.owner == "P2" && square7.owner == "P2"){
-			$scope.winnerTwo = true;
-			$scope.isDisabled = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
+			controller.game.winnerTwo = true;
+			controller.game.isDisabled = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
 			square3.color = square5.color = square7.color = true;
 		} 
 		else if ((square1.owner == "P1" || square1.owner == "P2") && (square2.owner == "P1" || square2.owner == "P2") && (square3.owner == "P1" || square3.owner == "P2") && (square4.owner == "P1" || square4.owner == "P2") && (square5.owner == "P1" || square5.owner == "P2") && (square6.owner == "P1" || square6.owner == "P2") && (square7.owner == "P1" || square7.owner == "P2") && (square8.owner == "P1" || square8.owner == "P2") && (square9.owner == "P1" || square9.owner == "P2")){
-			$scope.catsGame = true;
-			$scope.yourTurnOne = false;
-			$scope.yourTurnTwo = false;
-			$scope.isDisabled = true;
-		}
-
-		
-		// $scope.game.$save();
-	};
+			controller.game.catsGame = true;
+			controller.game.yourTurnOne = false;
+			controller.game.yourTurnTwo = false;
+			controller.game.isDisabled = true;
+		}		
+			controller.game.$save();
+		};
 
 	// -------- Color the Winning Combinations -------- //
 
-
 //-------When "play again" button is pressed, the board gets cleared with this function--------------------//
 
-	$scope.playAgain = function(square1, square2, square3, square4, square5, square6, square7, square8, square9) {
+	function playAgain() {
 
-		$scope.isDisabled = true;
-		$scope.winnerOne = false;
-		$scope.winnerTwo = false;
-		$scope.catsGame = false;
-		$scope.yourTurnOne = false;
-		$scope.yourTurnTwo = false;
-		$scope.formOne = false;
-		$scope.formTwo = false;
-		
-		$scope.Player_Name_One = "";
-		$scope.Player_Name_Two = "";
+		var keepPlayerOne = controller.game.Player_Name_One;
+		var keepPlayerTwo = controller.game.Player_Name_Two;
 
-		$scope.inputOne = "";
-		$scope.inputTwo = "";
+		newGame();
 
-		$scope.squares = [
-		[{owner:""},{owner:""},{owner:""}],
-		[{owner:""},{owner:""},{owner:""}],
-		[{owner:""},{owner:""},{owner:""}]
-		];
+		controller.game.Player_Name_One = keepPlayerOne;
+		controller.game.Player_Name_Two = keepPlayerTwo;
+		controller.game.helloOne = true;
+		controller.game.helloTwo = true;
+		controller.game.formOne = true;
+		controller.game.formTwo = true;
+
+		controller.game.$save();
 	};
 
-			// 	$scope.gameOver = false;
+			// 	controller.game.gameOver = false;
 			// $scope.newPlayers = angular.copy($scope.players);
 			// $scope.newBoard = angular.copy($scope.gameContainer);
 
 //-------When "reset game" button is pressed, the board gets cleared with this function--------------------//
 
-	$scope.resetGame = function(square1, square2, square3, square4, square5, square6, square7, square8, square9) {
+	function resetGame() {
 
-		$scope.isDisabled = true;
-		$scope.winnerOne = false;
-		$scope.winnerTwo = false;
-		$scope.catsGame = false;
-		$scope.yourTurnOne = false;
-		$scope.yourTurnTwo = false;
-		$scope.formOne = false;
-		$scope.formTwo = false;
-		
-		$scope.Player_Name_One = "";
-		$scope.Player_Name_Two = "";
+		newGame();
 
-		$scope.inputOne = "";
-		$scope.inputTwo = "";
-
-		$scope.squares = [
-		[{owner:""},{owner:""},{owner:""}],
-		[{owner:""},{owner:""},{owner:""}],
-		[{owner:""},{owner:""},{owner:""}]
-		];
+		controller.inputOne = "";
+		controller.inputTwo = "";
+		controller.game.$save();
 	};
-}]);
+
+};
+
